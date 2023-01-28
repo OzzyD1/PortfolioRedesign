038624a1bc17d6989c8eb69ddbb84b452ee9655e
OzzyD1 <ozzydomarkas@gmail.com>
2023-01-28 14:12:21 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// let scrollDown = document.querySelector('.scroll-down');
+
+// scrollDown.addEventListener('click', function() {
+//   let nextSection = document.querySelector('#next-section');
+//   nextSection.scrollIntoView({ 
+//     behavior: 'smooth' 
+//   });
+// });
